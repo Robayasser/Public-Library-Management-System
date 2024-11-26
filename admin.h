17dca9a940993b4f05c19ca12037320656df0345
Robayasser <robayasser@aucegypt.edu>
2024-11-26 13:58:56 +0200

    Update admin.h

--- admin.h
+++ admin.h
@@ -1,19 +1,19 @@
-#ifndef ADMIN_H
-#define ADMIN_H
+#ifndef ADMIN_H //to include this file once
+#define ADMIN_H 
 
 #include "book.h"
 #include "seminar.h"
 #include "user.h"
 #include <map> // Include the map header
-#include <string>
-#include <vector>
+#include <string> // for text
+#include <vector> // manage lists 
 
 class Admin {
 public:
-  void manageBooks(std::vector<Book> &books);
-  void manageSeminars(std::vector<Seminar> &seminars);
+  void manageBooks(std::vector<Book> &books); //admin handle books
+  void manageSeminars(std::vector<Seminar> &seminars); // handles seminar 
   void manageUsers(
-      std::map<std::string, User> &userMap); // Placeholder for user management
+      std::map<std::string, User> &userMap); // handles user 
 };
 
 #endif
